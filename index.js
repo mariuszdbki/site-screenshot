a0b6c07acb8631eca6ba5bbab127c5505fffe0c9
Mariusz Dąbrowski <mariuszdbki@gmail.com>
2024-04-16 23:30:23 +0200

    ~ rewritten webdriver creation procedure + added method of providing url in message body (as json)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const webdriver = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 const app = express();
+
 const httpPort = 3300;
 
 const seleniumUrl = process.env.SELENIUM_URL; // e.g. http://selenium.grid:4444/wd/hub
@@ -9,38 +10,68 @@ const websiteUrl = process.env.WEBSITE_URL;
 const browserWindowWidth = process.env.BROWSER_WINDOW_WIDTH || 600;
 const browserWindowHeight = process.env.BROWSER_WINDOW_HEIGHT || 800;
 
-app.get('/:url?', async (req, res) => {
-    
-    let url = req.params.url || websiteUrl;
-    if (!url) {
-        res.status(404).send('No URL provided!'+ url);
-        return;
-    }
+const firefoxOptions = new firefox.Options();
+    firefoxOptions.addArguments('--headless');
+    firefoxOptions.addArguments(`--width=${browserWindowWidth}`);
+    firefoxOptions.addArguments(`--height=${browserWindowHeight}`);
 
-    let options = new firefox.Options();
-    options.addArguments([
-        '--headless',
-        `--width=${browserWindowWidth}`,
-        `--height=${browserWindowHeight}`
-    ]);
+let driver;
 
-    const driver = await new webdriver.Builder()
-        .forBrowser(webdriver.Browser.FIREFOX)
-        .usingServer(seleniumUrl)
-        .setFirefoxOptions(options)
-        .build();
+async function createDriver() {
+    driver = await new webdriver.Builder()
+    .forBrowser(webdriver.Browser.FIREFOX)
+    .usingServer(seleniumUrl)
+    .setFirefoxOptions(firefoxOptions)
+    .build();
+}
 
-    await driver.manage().setTimeouts({ implicit: 5000 }); // 5s
-    
-    await driver.get(url);
-    let image = await driver.takeScreenshot(true); 
-    await driver.quit();
+async function serveRequest(req, res) {
+    try {
+        if (!driver) {
+            console.error('Selenium WebDriver not initialized!');
+            res.status(500).send('Failed to take screenshot (webdriver not initialized)');
+            return;
+        }
 
-    res.set('Content-Type', 'image/png');
-    res.send(Buffer.from(image, 'base64'));
-    
+        let url = req.body?.url || websiteUrl;
+        if (!url) {
+            res.status(404).send('No proper URL provided!');
+            return;
+        }
+
+        await driver.get(url);
+        let image = await driver.takeScreenshot(true);
+
+        res.set('Content-Type', 'image/png');
+        res.send(Buffer.from(image, 'base64'));
+    } catch (error) {
+        console.error('Failed to take screenshot:', error);
+        res.status(500).send('Failed to take screenshot');
+    }   
+}
+
+createDriver().catch(error => {
+    console.error('Failed to initialize Selenium WebDriver:', error);
+    process.exit(1); 
 });
+
+app.use(express.json());
+
+app.all('/', serveRequest);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status).send(err.message);
+  })
 
 app.listen(httpPort, () => {
     console.log(`Listening on port ${httpPort}!`);
+});
+
+process.on('SIGINT', async () => {
+    console.log('Shutting down...');
+    if (driver) {
+        await driver.quit();
+    }
+    process.exit(0);
 });
